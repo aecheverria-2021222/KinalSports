166a6d74e86cd307f0d61e7282737cda21cb4268
aecheverria-2021222 <aecheverria-2021222@kinal.edu.gt>
2026-04-24 11:52:27 -0600

    fix: Cambiar getAllUsers en handleCreate en Users.jsx

--- authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Application/obj/Debug/net8.0/AuthService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3479e696bf7a257a315c3e0b8b748a8f0c31090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b59d81f8a62c939ea220aa9cf72b1e9472bb461")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
