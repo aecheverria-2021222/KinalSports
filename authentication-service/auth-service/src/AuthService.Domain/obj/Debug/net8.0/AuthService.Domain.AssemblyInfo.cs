fc12db7d128e966cba0b8108101c9986a0747e69
aecheverria-2021222 <aecheverria-2021222@kinal.edu.gt>
2026-04-27 10:23:43 -0600

    chore: Cambiar Puerto de frontedUrl

--- authentication-service/auth-service/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
+++ authentication-service/auth-service/src/AuthService.Domain/obj/Debug/net8.0/AuthService.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73f205113a3ce5e8e516c44f54b597b9c6ab129d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26cc0a0111387c06074d556b3a1c2c0c45806f44")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
